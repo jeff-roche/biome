0ed882ba03423909582b8e6c0cd38d466a4016a4
Jeff Roche <jeff.roche@meltwater.com>
2022-06-02 08:46:50 -0400

    fix: cleaned up the flag, added the short form, and display os and arch with version

--- main.go
+++ main.go
@@ -10,17 +10,25 @@ import (
 	"github.com/jeff-roche/biome/parser"
 )
 
-var Version string = "v0.X.Y - installed via go install"
+var Version string
 
 func main() {
+	var versionFlag bool
+	flag.BoolVar(&versionFlag, "version", false, "Display the current version of this utility")
+	flag.BoolVar(&versionFlag, "v", false, "Display the current version of this utility")
+
 	biomeName := flag.String("b", "", "[Required] The name of the biome to use")
-	versionFlag := flag.Bool("version", false, "Display the current version")
 
 	flag.Parse()
 
 	// If they want to print the version, just do that
-	if *versionFlag {
-		fmt.Println(Version)
+	if versionFlag {
+		if Version == "" {
+			fmt.Println("v0.X - installed via go install")
+		} else {
+			fmt.Println(Version)
+		}
+
 		return // We're done here
 	}
 
