0f5a7ca98bb5a2e3d3fd401c61bf9f4d10ff8112
Jeff Roche <jeff.roche@meltwater.com>
2022-07-08 11:05:07 -0400

    feat: command line secrets

--- src/lib/setters/cli_setter.go
+++ src/lib/setters/cli_setter.go
@@ -6,9 +6,12 @@ import (
 	"io"
 	"os"
 	"strings"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const CLI_ENVIRONMENT_SETTER_KEY = "from_cli"
+const CLI_ENVIRONMENT_SECRET_SETTER_KEY = "is_secret"
 
 // CLIEnvironmentSetter will pull an env var from the provided io.Reader (stdin)
 type CLIEnvironmentSetter struct {
@@ -16,15 +19,21 @@ type CLIEnvironmentSetter struct {
 	Value string
 }
 
-func NewCLIEnvironmentSetter(key string, rd io.Reader) (*CLIEnvironmentSetter, error) {
+func NewCLIEnvironmentSetter(key string, rd io.Reader, isSecret bool) (*CLIEnvironmentSetter, error) {
 	// Get the value from the io.Reader
 	fmt.Printf("%s: ", key)
-	reader := bufio.NewReader(rd)
-	val, err := reader.ReadString('\n')
+	var val string
+	var err error
+
+	if isSecret {
+		val, err = getSecretCliInput(rd)
+	} else {
+		val, err = getCliInput(rd)
+	}
+
 	if err != nil {
 		return nil, err
 	}
-	val = strings.Replace(val, "\n", "", -1) // Remove any newline characters
 
 	// Save and return the setter
 	return &CLIEnvironmentSetter{
@@ -35,4 +44,24 @@ func NewCLIEnvironmentSetter(key string, rd io.Reader) (*CLIEnvironmentSetter, e
 
 func (s CLIEnvironmentSetter) SetEnv() error {
 	return os.Setenv(s.Key, s.Value)
+}
+
+func getCliInput(rd io.Reader) (string, error) {
+	reader := bufio.NewReader(rd)
+	val, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	val = strings.Replace(val, "\n", "", -1) // Remove any newline characters
+
+	return val, nil
+}
+
+func getSecretCliInput(rd io.Reader) (string, error) {
+	byteSecret, err := terminal.ReadPassword(0)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Replace(string(byteSecret), "\n", "", -1), nil // Convert to string and remove any newline characters
 }
